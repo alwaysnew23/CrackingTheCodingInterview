0b610e9c066fdd6d030e6071ac79e255398cba50
alwaysnew23 <72125579+alwaysnew23@users.noreply.github.com>
2022-07-05 15:56:50 +0800

    determine if a string has all unique characters.

--- IsUniqueChars.java
+++ IsUniqueChars.java
@@ -1,22 +1,28 @@
+/* Author: Vincent X
+ * Date: July 5, 2022
+ * This program implements an algorithm to determine if a string 
+ * has all unique characters. 
+ */
+
 public class IsUniqueChars {
-    public static void main(String args[]) {
-        String s = "It's sunny";
-        
-        System.out.println(isUniqueChars(s));
+    public static void main(String[] args) {
+        System.out.println(isUniqueChars("great"));
+        System.out.println(isUniqueChars("high"));
     }
 
     public static boolean isUniqueChars(String str) {
-        if (str.length() > 128) return false;
-
-        boolean[] char_set = new boolean[128]; // default is false
-
+        if (str.length() > 128) {
+            return false;
+        }
+        boolean[] char_set = new boolean[128];
         for (int i = 0; i < str.length(); i++) {
             int val = str.charAt(i);
-
-            // Already found this char in string
-            if (char_set[val]) return false;
+            // already found this char in string
+            if (char_set[val]) {
+                return false;
+            }
             char_set[val] = true;
         }
-        return true;        
+        return true;
     }
 }
